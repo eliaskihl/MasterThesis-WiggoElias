ef8e7bee913cf9cded5b18129508e6b9292093ef
Wiggo Klapp <wiggo.klapp@hotmail.se>
2025-02-24 10:17:45 +0100

    change

--- run_ids/run_ids.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def run_suricata(pcap_file):
-    """Run Suricata on the provided PCAP file."""
-    cmd = ["sudo", "suricata", "-r", pcap_file, "-l", "../logs/suricata"]
-    subprocess.run(cmd, check=True)
-
-if __name__ == "__main__":
-    run_suricata("../dataset_loader/datasets/TII-SSRC-23/pcap/malicious/bruteforce/bruteforce_ftp.pcap")
